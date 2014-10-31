045dfa794ba90ef24a1d29d218231e9286bb69e8
Steven L. Markstrom <markstro@usgs.gov>
2014-10-31 19:34:28 +0000

    Fixes the par_name file error for max, min, default on linux
    git-svn-id: http://internalbrr.cr.usgs.gov/svn_MOWS/PRMS_SVN/mmf/trunk@6963 6316d359-22eb-4c0b-9c0f-3bdc8ec1e293

--- print_params.c
+++ print_params.c
@@ -130,9 +130,14 @@ int print_params (void) {
 
     switch(param->type) {
        case M_LONG:
+		   /* DANGER
           (void)fprintf (param_file, "Max       : %ld\n", *(long *)(param->max));
           (void)fprintf (param_file, "Min       : %ld\n", *(long *)(param->min));
           (void)fprintf (param_file, "Default   : %ld\n", *(long *)(param->def));
+		  */
+		  (void)fprintf (param_file, "Max       : %ld\n", *(int *)(param->max));
+          (void)fprintf (param_file, "Min       : %ld\n", *(int *)(param->min));
+          (void)fprintf (param_file, "Default   : %ld\n", *(int *)(param->def));
           break;
 
        case M_FLOAT:
