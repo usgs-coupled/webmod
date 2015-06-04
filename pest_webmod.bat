f67478cfed3afd487d2dd6eb3b058e23df6892f2
David L Parkhurst <dlpark@usgs.gov>
2015-06-04 18:47:32 +0000

    Removing files replaced by CMake. Old VS files.
    git-svn-id: svn://136.177.114.72/svn_GW/webmod/trunk/pest/andcrk@9774 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- pest_webmod.bat
+++
@@ -1,6 +0,0 @@
-rm -f andcrk.params
-rm -f andcrk.dat.pqi
-rm -f andcrk.statvar
-par2par par2par_andcrk.dat
-/home/rmwebb/programs/webmod-trunk/webmod_1.0.exe -C/home/rmwebb/andcrk_pest3/andcrk.control > webmod.log
-/home/dlpark/WEB_PEST/tsproc.exe < /home/rmwebb/andcrk_pest3/tsproc.in
