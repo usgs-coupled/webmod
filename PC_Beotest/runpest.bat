d0f4ef0e7cb57955828a1ecc851aa9ed2c599b56
David L Parkhurst <dlpark@usgs.gov>
2015-06-11 12:48:39 +0000

    Need final touchup for generality.
    git-svn-id: svn://136.177.114.72/svn_GW/webmod/trunk/pest/andcrk@9832 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- PC_Beotest/runpest.bat
+++ PC_Beotest/runpest.bat
@@ -1,4 +1,4 @@
-REM @echo off
+@echo off
 REM Sychronize latest WEBMOD executable, create the tsproc and PEST command files, and start beopest as master
 REM Help printed if command entered with no arguments
 IF %1.==. GOTO usage
@@ -11,7 +11,7 @@ REM Set variables
 set nodes=%1
 set pst=%2
 set "WEB_DAT=%HOME_BATCH%\..\WEBDATA"
-set "WEB_TPL=..\WEBTPL"
+set "WEB_TPL=%HOME_BATCH%\..\WEBTPL"
 set "WEB_SRC=.\webfiles"
 set "PROJECT_DIR_PATH=%HOME_BATCH%\webmod"
 set "PROJECT_DIR=%HOME%/webmod"
@@ -19,35 +19,43 @@ set "PEST_BIN_DIR_PATH=%HOME_BATCH%\..\..\PC_bin"
 set "PEST_BIN_DIR=%HOME%/../../PC_bin"
 set "TSPROC_BIN_DIR=%HOME%/../../PC_bin"
 
-REM dir
+REM set port number
 set PORT=4004 
 
-REM setup WEB_SRC
-if exist %WEB_SRC% rmdir /s/q %WEB_SRC%
-mkdir %WEB_SRC%
-REM sed is on NIXE so use it here. VB scripts are an alternative
-sed "s#@PROJECT_DIR@#%PROJECT_DIR%/#" %WEB_TPL%\andcrk_tsproc.tpl > %WEB_SRC%\andcrk_tsproc.dat
-sed "s#@PROJECT_DIR@#%PROJECT_DIR%/#g" %WEB_TPL%\pest_webmod.bat.tpl > %WEB_SRC%\pest_webmod.bata
-sed "s#@PEST_BIN_DIR@#%PEST_BIN_DIR%/#g" %WEB_SRC%\pest_webmod.bata > %WEB_SRC%\pest_webmod.batb
-sed "s#@TSPROC_BIN_DIR@#%TSPROC_BIN_DIR%/#" %WEB_SRC%\pest_webmod.batb > %WEB_SRC%\pest_webmod.bat
-sed -i "s#@DEL@#DEL#" %WEB_SRC%\pest_webmod.bat
-del %WEB_SRC%\pest_webmod.bata %WEB_SRC%\pest_webmod.batb
-copy %WEB_DAT%\* %WEB_SRC%
-copy %PEST_BIN_DIR_PATH%\webmod_1.0.exe %WEB_SRC%
-
-REM setup working directory PROJECT_DIR_PATH
+REM setup working directory PROJECT_DIR_PATH=webmod
 if exist %PROJECT_DIR_PATH% rmdir /s/q %PROJECT_DIR_PATH%
 mkdir %PROJECT_DIR_PATH%
-copy %WEB_SRC%\andcrk.statvar.full %PROJECT_DIR_PATH%\andcrk.statvar
-XCOPY /I %WEB_SRC% %PROJECT_DIR_PATH%
+
+REM Sed to make andcrk_tsproc.dat 
+sed "s#@PROJECT_DIR@#%PROJECT_DIR%/#" %WEB_TPL%\andcrk_tsproc.tpl > %PROJECT_DIR%\andcrk_tsproc.dat
+sed -i "s#@PROJECT_DIR@#%PROJECT_DIR%/#" %PROJECT_DIR%\andcrk_tsproc.dat
+
+REM Sed to make pest_webmod.bat in PROJECT_DIR
+sed "s#@PROJECT_DIR@#%PROJECT_DIR%/#g" %WEB_TPL%\pest_webmod.bat.tpl > %PROJECT_DIR_PATH%\pest_webmod.bat
+sed -i "s#@PEST_BIN_DIR@#%PEST_BIN_DIR%/#g" %PROJECT_DIR_PATH%\pest_webmod.bat
+sed -i "s#@TSPROC_BIN_DIR@#%TSPROC_BIN_DIR%/#" %PROJECT_DIR_PATH%\pest_webmod.bat
+sed -i "s#@DEL@#DEL#" %PROJECT_DIR_PATH%\pest_webmod.bat
+REM remove sed files
+rm sed*
+
+REM Copy files to tsproc directory
+copy %WEB_DAT%\*.ssf            	%PROJECT_DIR_PATH%
+copy %WEB_DAT%\pest_groups*.txt 	%PROJECT_DIR_PATH%
+copy %WEB_DAT%\pest_params*.txt 	%PROJECT_DIR_PATH%
+copy %WEB_DAT%\tsproc.in        	%PROJECT_DIR_PATH%
+copy %WEB_DAT%\andcrk.statvar.full	%PROJECT_DIR_PATH%\andcrk.statvar
+copy %WEB_DAT%\par2par_andcrk.tpl	%PROJECT_DIR_PATH%
 
 REM run tsproc
 cd %PROJECT_DIR_PATH%
-%PEST_BIN_DIR_PATH%\tsproc.exe < %PROJECT_DIR_PATH%\tsproc.in
-cd ..
+%PEST_BIN_DIR_PATH%\tsproc.exe < .\tsproc.in
 
+REM move output from tsproc to working directory
+cp %TSPROC_BIN_DIR%\webmod_1.0.exe  %PROJECT_DIR_PATH%
+cp %WEB_DAT%\andcrk.control         %PROJECT_DIR_PATH%
+
+REM change tsproc mode from "pest_prep" to "model_run"
 sed -i "2s/CONTEXT pest_prep/CONTEXT model_run/" %PROJECT_DIR_PATH%\andcrk_tsproc.dat
-
 REM Line 6 of the pst file RLAMBDA1 .....	
 sed -i "6s/.*/10.0  -3.0    0.3    0.03     -10  999  LAMFORGIVE/" %PROJECT_DIR_PATH%\andcrk.pst
 REM Line 7 of the pst file RELPARMAX FACPARMAX FACORIG.....	
@@ -58,41 +66,34 @@ REM Line 13. SVD block MAXSING EIGTHRESH. Replace MAXSING the maximum number of 
 sed -i "13s/.*/19 5e-7/" %PROJECT_DIR_PATH%\andcrk.pst
 REM Line 14 EIGWRITE. 0 if not using SVD output file
 sed -i "14s/.*/1/" %PROJECT_DIR_PATH%\andcrk.pst
-
 REM remove sed files
 rm sed*
+cd %PROJECT_DIR_PATH%
 
 REM Run parallel pest Master
 cd %PROJECT_DIR_PATH%
 start "Master" cmd /k "call %PEST_BIN_DIR_PATH%\beopest64 %PROJECT_DIR_PATH%\%pst% /H :%PORT% & cd .."
 
-sleep 5
+REM wait 2 seconds for master to come up before starting workers
+sleep 2
 
-REM start "Workers" cmd /k call .\slaves\runslave.bat %nodes% %pst%
-REM goto :EOF
-
-REM Run parallel pest workers
+REM make tmp dirs and run parallel pest workers
 set MASTER=%COMPUTERNAME%
-set WEBDIR=%PROJECT_DIR_PATH%\
-set WORKER_DIR=%PROJECT_DIR_PATH%\
-
-REM make tmp dirs where slaves will be activated.
 for /l %%X in (1, 1, %nodes%) do (
-if exist %WORKER_DIR%\tmpest%%X rmdir /s/q %WORKER_DIR%\tmpest%%X 
-mkdir %WORKER_DIR%\tmpest%%X
-cd %WORKER_DIR%\tmpest%%X
-copy %WEBDIR%andcrk.pst .\
-copy %WEBDIR%params_andcrk.tpl .\
-copy %WEBDIR%pqi_andcrk.tpl .\
-copy %WEBDIR%pest_webmod.bat .\
-copy %WEBDIR%andcrk.dat .\
-copy %WEBDIR%andcrk.dat.chemdat .\
-copy %WEBDIR%phreeq_lut .\
-copy %WEBDIR%phreeqc_web_lite.dat .\
-copy %WEBDIR%andcrk.ins .\
-copy %WEBDIR%andcrk_tsproc.dat .\
-START /B %PEST_BIN_DIR_PATH%\beopest64 %WEBDIR%%pst% /H %MASTER%:%PORT%
-cd %WORKER_DIR%\
+	if exist %PROJECT_DIR_PATH%\tmpest%%X rmdir /s/q %PROJECT_DIR_PATH%\tmpest%%X 
+	mkdir %PROJECT_DIR_PATH%\tmpest%%X
+	cd %PROJECT_DIR_PATH%\tmpest%%X
+	copy %WEB_DAT%\params_andcrk.tpl .\
+	copy %WEB_DAT%\pqi_andcrk.tpl .\
+	copy %WEB_DAT%\andcrk.dat .\
+	copy %WEB_DAT%\andcrk.dat.chemdat .\
+	copy %WEB_DAT%\phreeq_lut .\
+	copy %WEB_DAT%\phreeqc_web_lite.dat .\
+	copy %PROJECT_DIR_PATH%\pest_webmod.bat .\
+	copy %PROJECT_DIR_PATH%\andcrk.ins .\
+	copy %PROJECT_DIR_PATH%\andcrk_tsproc.dat .\
+	START /B %PEST_BIN_DIR_PATH%\beopest64 %PROJECT_DIR_PATH%\%pst% /H %MASTER%:%PORT%
+	cd %PROJECT_DIR_PATH%
 )
 cd %PROJECT_DIR_PATH%\..
 GOTO :EOF
