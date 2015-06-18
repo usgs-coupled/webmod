d557b79ad5c6cd7543046a56618a94da624c24b8
David L Parkhurst <dlpark@usgs.gov>
2015-06-18 15:27:37 +0000

    New names for most input files for pest. Revised runpest.bat, only takes one argument, uses new file names. Need to update Linux versions.
    git-svn-id: svn://136.177.114.72/svn_GW/webmod/trunk@9856 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- pest/andcrk_pest/runpest.bat
+++ pest/andcrk_pest/runpest.bat
@@ -1,17 +1,13 @@
 @echo off
 REM Sychronize latest WEBMOD executable, create the tsproc and PEST command files, and start beopest as master
 REM Help printed if command entered with no arguments
-IF %1.==. GOTO usage
-IF %2.==. GOTO NoCase
-
-set HOME=%cd%
-set HOME_SED=%HOME:\=/%
+IF %1.==. GOTO :USAGE
 
 REM Set variables
-set nodes=%1
-set pst=%2
-set BASIN=andcrk
-
+set NODES=%1
+set PST=webmod.pst
+set HOME=%cd%
+set HOME_SED=%HOME:\=/%
 set "INPUT_DIR=%HOME%\input"
 set INPUT_DIR_SED=%INPUT_DIR:\=/%
 set "PEST_FILES_DIR=%HOME%\pest_files"
@@ -32,61 +28,49 @@ REM setup working directory PROJECT_DIR=pest_run_dir
 if exist %PROJECT_DIR% rmdir /s/q %PROJECT_DIR%
 mkdir %PROJECT_DIR%
 
-REM Sed to make basin_tsproc.dat 
-call %HOME%\sed.bat @PROJECT_DIR@ %PROJECT_DIR_SED%/ %PEST_FILES_DIR%\%BASIN%_tsproc.tpl > %PROJECT_DIR%\%BASIN%_tsproc.dat
+REM Sed to make tsproc.dat 
+call %HOME%\sed.bat @PROJECT_DIR@ %PROJECT_DIR_SED%/ %PEST_FILES_DIR%\tsproc.dat.tpl > %PROJECT_DIR%\tsproc.dat
 
 REM Sed to make pest_webmod.bat in PROJECT_DIR
 call %HOME%\sed.bat @PROJECT_DIR@ %PROJECT_DIR_SED%/       %PEST_FILES_DIR%\pest_webmod.bat.tpl > %PROJECT_DIR%\pest_webmod.bat1
 call %HOME%\sed.bat @PEST_BIN_DIR@ %PEST_BIN_DIR_SED%/     %PROJECT_DIR%\pest_webmod.bat1 >       %PROJECT_DIR%\pest_webmod.bat2
 call %HOME%\sed.bat @TSPROC_BIN_DIR@ %TSPROC_BIN_DIR_SED%/ %PROJECT_DIR%\pest_webmod.bat2 >       %PROJECT_DIR%\pest_webmod.bat3
 call %HOME%\sed.bat @DEL@ DEL                              %PROJECT_DIR%\pest_webmod.bat3 >       %PROJECT_DIR%\pest_webmod.bat
-DEL %PROJECT_DIR%\pest_webmod.bat1 %PROJECT_DIR%\pest_webmod.bat2 %PROJECT_DIR%\pest_webmod.bat3
+DEL %PROJECT_DIR%\pest_webmod.bat1 
+DEL %PROJECT_DIR%\pest_webmod.bat2 
+DEL %PROJECT_DIR%\pest_webmod.bat3
 
 REM Copy files to tsproc directory
 copy %PEST_FILES_DIR%\*.ssf            	        %PROJECT_DIR%
 copy %PEST_FILES_DIR%\pest_groups*.txt 	        %PROJECT_DIR%
 copy %PEST_FILES_DIR%\pest_params*.txt 	        %PROJECT_DIR%
 copy %PEST_FILES_DIR%\tsproc.in         	%PROJECT_DIR%
-copy %PEST_FILES_DIR%\%BASIN%.statvar.full	%PROJECT_DIR%\%BASIN%.statvar
-copy %PEST_FILES_DIR%\par2par_%BASIN%.tpl	%PROJECT_DIR%
+copy %PEST_FILES_DIR%\webmod.statvar	        %PROJECT_DIR%\webmod.statvar
+copy %PEST_FILES_DIR%\par2par.dat.tpl	        %PROJECT_DIR%
 
 REM run tsproc
 cd %PROJECT_DIR%
 %PEST_BIN_DIR%\tsproc.exe < .\tsproc.in
 
 REM move output from tsproc to working directory
-COPY %TSPROC_BIN_DIR%\webmod_1.0.exe  %PROJECT_DIR%
+COPY %TSPROC_BIN_DIR%\webmod.exe  %PROJECT_DIR%
 
 REM edit the control file
-call %HOME%\sed.bat ./input . %CONTROL_DIR%\%BASIN%.control >    %PROJECT_DIR%\%BASIN%.control1
-call %HOME%\sed.bat ./output . %PROJECT_DIR%\%BASIN%.control1 >  %PROJECT_DIR%\%BASIN%.control2
-call %HOME%\sed.bat ../../bin . %PROJECT_DIR%\%BASIN%.control2 >  %PROJECT_DIR%\%BASIN%.control
-DEL %PROJECT_DIR%\%BASIN%.control1 %PROJECT_DIR%\%BASIN%.control2
-
+call %HOME%\sed.bat   ./input  .  %CONTROL_DIR%\webmod.control  >  %PROJECT_DIR%\webmod.control1
+call %HOME%\sed.bat  ./output  .  %PROJECT_DIR%\webmod.control1 >  %PROJECT_DIR%\webmod.control2
+call %HOME%\sed.bat ../../bin  .  %PROJECT_DIR%\webmod.control2 >  %PROJECT_DIR%\webmod.control
+DEL %PROJECT_DIR%\webmod.control1  
+DEL %PROJECT_DIR%\webmod.control2
 
 REM change tsproc mode from "pest_prep" to "model_run"
-call %HOME%\sed.bat "CONTEXT pest_prep" "CONTEXT model_run" %PROJECT_DIR%\%BASIN%_tsproc.dat > %PROJECT_DIR%\xxx
-MOVE  %PROJECT_DIR%\xxx %PROJECT_DIR%\%BASIN%_tsproc.dat
-
-
-REM Line 6 of the pst file RLAMBDA1 .....	
-rem sed -i "6s/.*/10.0  -3.0    0.3    0.03     -10  999  LAMFORGIVE/" %PROJECT_DIR_PATH%\%BASIN%.pst
-REM Line 7 of the pst file RELPARMAX FACPARMAX FACORIG.....	
-rem sed -i "7s/.*/0.2   2.0   1.0e-3/" %PROJECT_DIR_PATH%\%BASIN%.pst
-REM Line 9 of the pst file. NOPTMAX Max # of optimizations. Default is 30, set to 0 for single run with phi contributions, 1 for sensitivities, or a small number to test PEST loops.
-rem sed -i "9s/.*/30   .005  4   4  .005   4/" %PROJECT_DIR_PATH%\%BASIN%.pst
-REM Line 13. SVD block MAXSING EIGTHRESH. Replace MAXSING the maximum number of adjustable variables (number of singlular valuess at which truncation occurs)
-rem sed -i "13s/.*/19 5e-7/" %PROJECT_DIR_PATH%\%BASIN%.pst
-REM Line 14 EIGWRITE. 0 if not using SVD output file
-rem sed -i "14s/.*/1/" %PROJECT_DIR_PATH%\%BASIN%.pst
-REM remove sed files
-rem rm sed*
+call %HOME%\sed.bat "CONTEXT pest_prep" "CONTEXT model_run" %PROJECT_DIR%\tsproc.dat > %PROJECT_DIR%\xxx
+MOVE  %PROJECT_DIR%\xxx %PROJECT_DIR%\tsproc.dat
 
 REM Edit .pst file for parameter estimation
 SETLOCAL ENABLEEXTENSIONS
 SETLOCAL ENABLEDELAYEDEXPANSION
-set /a _COUNTER=0
-set pstfile=%PROJECT_DIR%\%BASIN%.pst
+set /a COUNTER=0
+set pstfile=%PROJECT_DIR%\webmod.pst
 set tmppest=%PROJECT_DIR%\tmp.pst
 for /f "tokens=1,* delims=]" %%A in ('"type %pstfile%|find /n /v """') do (
     set /a "COUNTER+=1"
@@ -109,62 +93,84 @@ for /f "tokens=1,* delims=]" %%A in ('"type %pstfile%|find /n /v """') do (
 )
 MOVE %tmppest% %pstfile%
 
-REM Run parallel pest Master
-cd %PROJECT_DIR%
-start "Master" cmd /k "call %PEST_BIN_DIR%\beopest64.exe %PROJECT_DIR%\%pst% /H /L :%PORT% & cd .. & echo Here I am."
-
-REM wait 2 seconds for master to come up before starting workers
-sleep 2
-
 REM make tmp dirs and run parallel pest workers
 set MASTER=%COMPUTERNAME%
-for /l %%X in (1, 1, %nodes%) do (
+for /l %%X in (1, 1, %NODES%) do (
 	if exist %PROJECT_DIR%\tmpest%%X rmdir /s/q %PROJECT_DIR%\tmpest%%X 
 	mkdir %PROJECT_DIR%\tmpest%%X
 	cd %PROJECT_DIR%\tmpest%%X
-	copy %PEST_FILES_DIR%\params_%BASIN%.tpl .\
-	copy %PEST_FILES_DIR%\pqi_%BASIN%.tpl .\
-	copy %INPUT_DIR%\%BASIN%.dat .\
-	copy %INPUT_DIR%\%BASIN%.dat.chemdat .\
-	copy %INPUT_DIR%\phreeq_lut .\
-	copy %INPUT_DIR%\phreeqc_web_lite.dat .\
-	copy %PROJECT_DIR%\pest_webmod.bat .\
-	copy %PROJECT_DIR%\%BASIN%.ins .\
-	copy %PROJECT_DIR%\%BASIN%_tsproc.dat .\
-	START /B %PEST_BIN_DIR%\beopest64.exe %PROJECT_DIR%\%pst% /H %MASTER%:%PORT% & cd %PROJECT_DIR%\..
+	copy %PEST_FILES_DIR%\webmod.params.tpl .\
+	copy %PEST_FILES_DIR%\webmod.pqi.tpl    .\
+	copy %INPUT_DIR%\webmod.hydro.dat       .\
+	copy %INPUT_DIR%\webmod.chem.dat        .\
+	copy %INPUT_DIR%\phreeq_lut             .\
+	copy %INPUT_DIR%\phreeqc_web_lite.dat   .\
+	copy %PROJECT_DIR%\pest_webmod.bat      .\
+	copy %PROJECT_DIR%\tsproc.sim.out.ins   .\
+	copy %PROJECT_DIR%\tsproc.dat           .\
+	START /B %PEST_BIN_DIR%\beopest64.exe %PROJECT_DIR%\%PST% /H %MASTER%:%PORT% & cd %PROJECT_DIR%\..
 	cd %PROJECT_DIR%\..
 )
 
-REM /L requires to be able to run in PROJECT_DIR_PATH 
+REM /L requires a run in PROJECT_DIR
 cd %PROJECT_DIR%
-copy %PEST_FILES_DIR%\params_%BASIN%.tpl .\
-copy %PEST_FILES_DIR%\pqi_%BASIN%.tpl .\
-copy %INPUT_DIR%\%BASIN%.dat .\
-copy %INPUT_DIR%\%BASIN%.dat.chemdat .\
-copy %INPUT_DIR%\phreeq_lut .\
-copy %INPUT_DIR%\phreeqc_web_lite.dat .\
-cd %PROJECT_DIR%\..
-echo ===================================================================================
+copy %PEST_FILES_DIR%\webmod.params.tpl .\
+copy %PEST_FILES_DIR%\webmod.pqi.tpl    .\
+copy %INPUT_DIR%\webmod.hydro.dat       .\
+copy %INPUT_DIR%\webmod.chem.dat        .\
+copy %INPUT_DIR%\phreeq_lut             .\
+copy %INPUT_DIR%\phreeqc_web_lite.dat   .\
+
+REM Run parallel pest Master
+cd %PROJECT_DIR%
+%PEST_BIN_DIR%\beopest64.exe %PROJECT_DIR%\%PST% /H /L :%PORT% 
+
+REM tidy up
+IF "%ERRORLEVEL%" == "0" (
+    if exist %PROJECT_DIR%\..\pest_results rmdir /s/q %PROJECT_DIR%\..\pest_results
+    mkdir %PROJECT_DIR%\..\pest_results
+    cd %PROJECT_DIR%
+    copy tsproc.sim.out.ins ..\pest_results
+    copy webmod.jac         ..\pest_results
+    copy webmod.jco         ..\pest_results
+    copy webmod.jst         ..\pest_results
+    copy webmod.mtt         ..\pest_results
+    copy webmod.par         ..\pest_results
+    copy webmod.prf         ..\pest_results
+    copy webmod.pst         ..\pest_results
+    copy webmod.rec         ..\pest_results
+    copy webmod.rei         ..\pest_results
+    copy webmod.res         ..\pest_results
+    copy webmod.rmr         ..\pest_results
+    copy webmod.rsd         ..\pest_results
+    copy webmod.rst         ..\pest_results
+    copy webmod.sen         ..\pest_results
+    copy webmod.seo         ..\pest_results
+    copy webmod.svd         ..\pest_results
+    copy webmod.pqi         ..\pest_results
+    copy webmod.params      ..\pest_results
+    copy webmod.pqi         ..\input\webmod.pqi.pest
+    copy webmod.params      ..\input\webmod.params.pest
+    copy tsproc.dat         ..\pest_results
+    cd   %PROJECT_DIR%\..
+REM
+REM run sensitivity plots here
+REM
+) else (
+    echo Beopest failed.
+)
+
 REM Done.
+GOTO :FINISH_UP
+
+:USAGE
+echo.
+echo usage: runpest n 
+echo where  n is number of workers to start
 GOTO :EOF
 
-:usage
-echo.
-echo usage: runpest n case.pst
-echo where  n is number of slaves to start, and
-echo        case.pst is the name of the PEST control file.
-GOTO :EOF
+:FINISH_UP
 
-:NoCase
-echo.
-echo Missing name of PEST control file
-echo.
-echo usage: runslave n case.pst
-echo where  n is number of slaves to start, and
-echo        case.pst is the name of the PEST control file.
-:EOF
-
-echo DOES THIS EVER GET CALLED???????????????????????????????????????????????????????????????
-cd %PROJECT_DIR_PATH%\..
+cd %PROJECT_DIR%\..
 
 
--- pest/andcrk_pest/webmod.bat
+++ pest/andcrk_pest/webmod.bat
@@ -1,7 +1,7 @@
 echo %time%
 @ECHO OFF
-..\..\bin\webmod_1.0 -C.\control\webmod.control -print
-..\..\bin\webmod_1.0 -C.\control\webmod.control > cmd.out
+..\..\bin\webmod.exe -C.\control\webmod.control -print
+..\..\bin\webmod.exe -C.\control\webmod.control > webmod.log
 ECHO.
 echo %time%
 ECHO Run complete. Please press enter to continue.
