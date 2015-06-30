67eabd4808933988f89ac82694438a92abf103f4
Richard M Webb <rmwebb@usgs.gov>
2015-06-30 00:12:26 +0000

    Moved all Icacos work into top level /projects directory. Distribution will just house static copies of the workspace that will be distributed with WEBMOD. Still need to create the "Andrews_tutorial" project area that is presented in the Quick Start of the manual.
    git-svn-id: svn://136.177.114.72/svn_GW/webmod/trunk@9911 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- Distribution/projects/Icacos/webmod.bat
+++
@@ -1,8 +0,0 @@
-echo %time%
-@ECHO OFF
-..\..\bin\webmod -C.\control\webmod.control -print
-..\..\bin\webmod -C.\control\webmod.control > cmd.out
-ECHO.
-echo %time%
-ECHO Run complete. Please press enter to continue.
-PAUSE>NUL
--- Distribution/projects/Icacos/webmod_paramtool.bat
+++
@@ -1,2 +0,0 @@
-..\..\bin\webmod -C.\control\webmod.control -print
-java -cp ../../lib/oui3.jar oui.paramtool.ParamTool ./input/webmod.params
