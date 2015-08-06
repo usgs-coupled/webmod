0762d181d21f50e9c12ca2feee4dc0064f78a1a4
Robert S. Regan <rsregan>
2015-08-06 16:56:07 +0000

    put back message about reaching the end of the Data File
    git-svn-id: http://internalbrr.cr.usgs.gov/svn_MOWS/PRMS_SVN/mmf/trunk@7577 6316d359-22eb-4c0b-9c0f-3bdc8ec1e293

--- read_line.c
+++ read_line.c
@@ -87,8 +87,9 @@ long read_line (void) {
 **  9999 in the year field is the code for EOF. 
 */
       if (cur_fd->time.year == 9999) {
-		  (void)fprintf (stderr,"9999  1 \n");
-         return ENDOFFILE;
+		  (void)fprintf (stderr,"\nWARNING, date of end_time reached the last date in the Data File \n");
+		  (void)fprintf(stderr, "         simulation stopped on: %ld %ld %ld \n", Mnowtime->year, Mnowtime->month, Mnowtime->day);
+	      return ENDOFFILE;
 	  }
 
 /*
