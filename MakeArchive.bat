a84a8d93948a49fc63d36cb7f49fe0c8048f3abd
David L Parkhurst <dlpark@usgs.gov>
2016-08-19 20:00:33 +0000

    Export from svn Edit directory names run to make archive
    git-svn-id: svn://136.177.114.72/svn_GW/webmod/trunk@11642 1feff8c3-07ed-0310-ac33-dd36852eb9cd

---
+++ MakeArchive.bat
@@ -0,0 +1,28 @@
+set ARCHIVE=C:\Programs\webmod-trunk\webmod1.0
+set EXPORTED=C:\Programs\webmod-trunk\webmod11641export
+DEL C:\Programs\webmod-trunk\webmod1.0 /Q /F
+mkdir C:\Programs\webmod-trunk\webmod1.0
+xcopy %EXPORTED% %ARCHIVE% /S /Y
+cd %ARCHIVE%
+DEL MakeArchive.bat
+RMDIR bin /s /q
+RMDIR lib /s /q
+RMDIR projects /s /q
+RMDIR tsproc-trunk /s /q
+cd IPhreeqcMMS\IPhreeqc
+del IPhreeqc*.* jenkins*.* Makefile.am vs*.* config* bootstrap All* INSTALL
+RMDIR unit  /s /q
+RMDIR tests  /s /q
+RMDIR testcpp  /s /q
+RMDIR test5  /s /q
+RMDIR test2  /s /q
+RMDIR test_ieee  /s /q
+RMDIR test  /s /q
+RMDIR R  /s /q
+RMDIR phreeqc3-examples  /s /q
+RMDIR packages /s /q
+RMDIR memory_leak_f  /s /q
+RMDIR examples  /s /q
+RMDIR build  /s /q
+RMDIR all /s /q
+cd %ARCHIVE%\..
