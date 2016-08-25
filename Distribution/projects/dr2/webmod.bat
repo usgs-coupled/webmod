61165583d2744cf88134e74f6b0eb35fab37f752
Richard M Webb <rmwebb@usgs.gov>
2016-08-25 20:09:03 +0000

    Update of DR2 batch and output to use latest executable
    git-svn-id: svn://136.177.114.72/svn_GW/webmod/trunk@11664 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- Distribution/projects/dr2/webmod.bat
+++ Distribution/projects/dr2/webmod.bat
@@ -1,7 +1,7 @@
 echo %time%
 @ECHO OFF
 ..\..\bin\webmod -C.\control\webmod.control -print
-..\..\bin\webmod -C.\control\webmod.control > webmod.log
+..\..\bin\webmod -C.\control\webmod.control
 ECHO.
 echo %time%
 ECHO Run complete. Please press enter to continue.
