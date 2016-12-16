635ac6101a30a5484539044bc4c33fcd0dfa53c9
Scott R Charlton <charlton@usgs.gov>
2016-12-16 05:11:51 +0000

    working on linux dist
    git-svn-id: svn://136.177.114.72/svn_GW/webmod/trunk@12097 1feff8c3-07ed-0310-ac33-dd36852eb9cd

---
+++ Distribution/projects/Andrews_tutorial/webmod_gui.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -cp ../../dist/oui4.jar:../../lib/AbsoluteLayout.jar:../../lib/jcommon-1.0.12.jar:../../lib/jfreechart-1.0.9.jar oui.mms.gui.Mms ./control/webmod.control
---
+++ Distribution/projects/Andrews_tutorial/webmod_paramtool.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -cp ../../dist/oui4.jar oui.paramtool.ParamTool ./input/webmod.params
