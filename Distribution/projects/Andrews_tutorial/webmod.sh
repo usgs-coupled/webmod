064e29a08ba65d7530d5bdb1ada805c0457bca1c
Scott R Charlton <charlton@usgs.gov>
2016-12-17 03:18:24 +0000

    working on linux dist
    git-svn-id: svn://136.177.114.72/svn_GW/webmod/trunk@12105 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- Distribution/projects/Andrews_tutorial/webmod.sh
+++ Distribution/projects/Andrews_tutorial/webmod.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+../../bin/webmod -C./control/webmod.control -print
 ../../bin/webmod -C./control/webmod.control
--- Distribution/projects/Andrews_tutorial/webmod_paramtool.sh
+++ Distribution/projects/Andrews_tutorial/webmod_paramtool.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 JARDIR=../../lib
+../../bin/webmod -C./control/webmod.control -print
 java -cp $JARDIR/oui4.jar oui.paramtool.ParamTool ./input/webmod.params
