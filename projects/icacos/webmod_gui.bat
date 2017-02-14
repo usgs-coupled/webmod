19ee44833607cac0f3fbbe0718c3070bd3d3e185
Richard M Webb <rmwebb@usgs.gov>
2017-02-14 18:03:55 +0000

    Update icacos project for sending to Heather Buss. Removed dimensions and parameters related to irrigation and updated to OUI4.
    git-svn-id: svn://136.177.114.72/svn_GW/webmod/trunk@12409 1feff8c3-07ed-0310-ac33-dd36852eb9cd

--- projects/icacos/webmod_gui.bat
+++ projects/icacos/webmod_gui.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
-..\..\bin\webmod_1.0 -C.\control\andcrk.control -print
-java -cp ..\..\lib\oui3.jar;..\..\lib\AbsoluteLayout.jar;..\..\lib\jcommon-1.0.12.jar;..\..\lib\jfreechart-1.0.9.jar oui.mms.gui.Mms .\control\webmod.control
+..\..\bin\webmod -C.\control\webmod.control -print
+java -cp ..\..\lib\oui4.jar;..\..\lib\AbsoluteLayout.jar;..\..\lib\jcommon-1.0.12.jar;..\..\lib\jfreechart-1.0.9.jar oui.mms.gui.Mms .\control\webmod.control
 ECHO.
 ECHO Run complete. Please press enter to continue.
 PAUSE>NUL
